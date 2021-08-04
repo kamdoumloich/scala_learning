54929286cfa596ac531463732406e85a568d5df2
Kamdoum <kamdoumloich@yahoo.fr>
2021-08-04 17:31:17 +0200

    variables

--- src/main/scala/project/first/Application.scala
+++ src/main/scala/project/first/Application.scala
@@ -3,6 +3,18 @@ package project.first
 object Application {
 
   def main(args: Array[String]): Unit = {
-    println("Bonjour tout le monde!")
+    // variable mit text
+    var text: String = "Bonjour tout le monde!"
+    println(text)
+    var var1 = 2
+
+    // variable, which the content is evaluated before the use
+    lazy val var2 = var1
+    var1 += 1
+    println(var1)
+    println(var2)
+
+    // constant
+    val constant = 2
   }
 }
